c6e560b0e81ae6d5610293939b000d1ff8f88166
James McEwen <jamesmcewen@users.noreply.github.com>
2022-06-28 12:32:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data.json";
+import BookForm from "./BookForm";
+import BookList from "./BookList";
 
-function App() {
+export default function App() {
+
+  const [bookList, setBookList] = useState(data);
+  const [filter, setFilter] = useState("All");
+
+  const addBook = (bookTitle) => {
+    const newBook = { id: bookList.length + 1, title: bookTitle, read: false };
+    const newList = [...bookList, newBook];
+    setBookList(newList);
+  }
+
+  const handleToggle = (id) => {
+    let mapped = bookList.map((book) => {
+      return book.id === Number(id)
+        ? { ...book, read: !book.read }
+        : { ...book }
+    });
+    setBookList(mapped);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookForm addBook={addBook} />
+      <BookList
+        bookList={bookList}
+        filter={filter}
+        setFilter={setFilter}
+        handleToggle={handleToggle}
+      />
     </div>
   );
 }
-
-export default App;
